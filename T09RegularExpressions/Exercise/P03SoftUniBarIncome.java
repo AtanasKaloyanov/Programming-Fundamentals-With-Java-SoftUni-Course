6edb5c1c62bf14f1aabe53aa89f51f15975f2165
AtanasKaloyanov <102095904+AtanasKaloyanov@users.noreply.github.com>
2022-03-24 21:06:25 +0200

    Update P03SoftUniBarIncome.java
    new solution to this task

--- T09RegularExpressions/Exercise/P03SoftUniBarIncome.java
+++ T09RegularExpressions/Exercise/P03SoftUniBarIncome.java
@@ -1,13 +1,36 @@
 package T09RegularExpressions.Exercise;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class P03SoftUniBarIncome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+         String regex = "%(?<customer>[A-Z][a-z]+)%[^|$.%]*?<(?<product>\\w+)>[^|$.%]*\\|(?<quantity>\\d+)\\|[^|$.%]*?(?<price>\\d+\\.?\\d+)\\$";
 
+        Pattern pattern = Pattern.compile(regex);
 
+        String input = scanner.nextLine();
+
+        double totalSum = 0;
+
+        while (!input.equals("end of shift")) {
+            String text = input;
+            Matcher matcher = pattern.matcher(text);
+
+            while (matcher.find()) {
+                int currentQuntity = Integer.parseInt(matcher.group("quantity"));
+                double currentPrice = Double.parseDouble(matcher.group("price"));
+                double currentSum = currentQuntity * currentPrice;
+
+                System.out.printf("%s: %s - %.2f%n", matcher.group("customer"), matcher.group("product"), currentSum);
+                totalSum += currentSum;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.printf("Total income: %.2f", totalSum);
     }
 }
 
