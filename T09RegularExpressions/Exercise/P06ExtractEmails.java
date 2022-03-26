b55b8b06f6fe69d8d4c231e5a85ae620912d8fe8
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-03-27 00:39:03 +0200

    I'm adding the final task on the topic Regular Expression.

--- T09RegularExpressions/Exercise/P06ExtractEmails.java
+++ T09RegularExpressions/Exercise/P06ExtractEmails.java
@@ -1,4 +1,20 @@
 package T09RegularExpressions.Exercise;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P06ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        String regex = "(?<=\\s)[a-zA-Z0-9]+([-.]\\w+)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\\.[a-z]{2,})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
 }
