4cf051df3077c0d70c26c85c55cd73ebf544e65e
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-03-29 15:24:25 +0300

    I'm beginning the preparation for the final exam of this course. It includes finding a solution of 15 problems. Now I'm adding the first one.

---
+++ ExamPreparation/E01FinalExamRetake15August2020/Demo.java
@@ -0,0 +1,14 @@
+package ExamPreparation.E01FinalExamRetake15August2020;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
+
