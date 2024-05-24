669f0610ded0dbacaf8bd00680e5a2f13fc6fb2e
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-05-24 18:10:24 +0300

    This is the updated solution of the task "Match Dates".

--- T09RegularExpressions/Lab/P02MatchPhoneNumber.java
+++ T09RegularExpressions/Lab/P02MatchPhoneNumber.java
@@ -7,24 +7,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class P02MatchPhoneNumber {
+    private static final String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
-
-
-        String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
         String text = scanner.nextLine();
 
+        // 2. Adding all matches in a list via Pattern and Matcher classes:
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
 
-       List<String> matcherList = new ArrayList<>();
+        List<String> matcherList = new ArrayList<>();
 
         while (matcher.find()) {
-           matcherList.add(matcher.group());
+            matcherList.add(matcher.group());
         }
 
-
+        // 3. Output printing
         System.out.print(matcherList.toString().replaceAll("[\\[\\]]", ""));
-        //System.out.print(String.join(", ", matcherList));
     }
 }
