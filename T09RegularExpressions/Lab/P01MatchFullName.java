849ff4170b13f58da53b7871b4248b5dea414582
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2024-05-24 18:06:18 +0300

    These are the updated lab tasks of the theme "Regular Expressions".

--- T09RegularExpressions/Lab/P01MatchFullName.java
+++ T09RegularExpressions/Lab/P01MatchFullName.java
@@ -5,12 +5,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class P01MatchFullName {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private static final String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
 
-        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+    public static void main(String[] args) {
+        // 1. Input reading
+        Scanner scanner = new Scanner(System.in);
         String givenText = scanner.nextLine();
 
+        // 2. Printing matches via Pattern and Matcher
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(givenText);
 
--- T09RegularExpressions/Lab/P03MatchDates.java
+++ T09RegularExpressions/Lab/P03MatchDates.java
@@ -5,19 +5,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class P03MatchDates {
+    private static final String regex = "(?<day>\\d{2})([\\/.-])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})";
     public static void main(String[] args) {
+        // 1. Input reading
         Scanner scanner = new Scanner(System.in);
-
-        String regex = "(?<day>\\d{2})([\\/.-])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})";
         String givenText = scanner.nextLine();
 
+        // 2. Finding the matches:
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(givenText);
 
         while (matcher.find()) {
-
-            System.out.printf("Day: %s, Month: %s, Year: %s%n", matcher.group("day"), matcher.group("month"), matcher.group("year"));
-
+            String currentDay = matcher.group("day");
+            String currentMonth = matcher.group("month");
+            String currentYear = matcher.group("year");
+            System.out.printf("Day: %s, Month: %s, Year: %s%n", currentDay, currentMonth, currentYear);
         }
 
     }
